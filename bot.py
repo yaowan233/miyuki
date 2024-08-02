905185a3fb30a63d6492906c613ad405897cfbca
yaowan233 <572473053@qq.com>
2024-08-02 10:36:37 +0800

    :sparkles: v0.1.1

--- bot.py
+++ bot.py
@@ -2,16 +2,12 @@ import nonebot
 from nonebot.adapters.satori import Adapter as SATORIAdapter
 
 
-
 nonebot.init()
-
 driver = nonebot.get_driver()
 driver.register_adapter(SATORIAdapter)
-
-nonebot.load_builtin_plugins('echo')
-
-
 nonebot.load_from_toml("pyproject.toml")
+nonebot.load_plugin("nonebot_plugin_alconna.builtins.plugins.help")
+
 
 if __name__ == "__main__":
     nonebot.run()
--- plugins/not_friend_no_reply/__init__.py
+++ plugins/not_friend_no_reply/__init__.py
@@ -1,8 +1,19 @@
 from nonebot.message import run_preprocessor
 from nonebot.exception import IgnoredException
+from nonebot.internal.adapter import Event
+from nonebot import Bot
+from nonebot.adapters.satori import MessageEvent
 
 
 @run_preprocessor
-async def preprocessor(event):
+async def preprocessor(event: Event, bot: Bot):
     if hasattr(event, 'message_type') and event.message_type == "private" and event.sub_type != "friend":
         raise IgnoredException("not reply group temp message")
+    if event.get_user_id() == bot.self_id:
+        raise IgnoredException("ignore bot message")
+
+
+@run_preprocessor
+async def _(event: MessageEvent):
+    if event.channel.id == '639617262':
+        raise IgnoredException("igonre group")
