f0ba860ea395a640acdbd32b6502698280d6e7d2
yaowan233 <572473053@qq.com>
2026-04-22 17:10:01 +0800

    v1.0.0

--- plugins/not_friend_no_reply/__init__.py
+++ plugins/not_friend_no_reply/__init__.py
@@ -2,10 +2,6 @@ from nonebot.message import run_preprocessor
 from nonebot.exception import IgnoredException
 from nonebot.internal.adapter import Event
 from nonebot import Bot
-from nonebot.adapters.satori import MessageEvent
-from nonebot.adapters.onebot.v11 import GroupMessageEvent as v11MessageEvent
-
-white_list = ['730139506', '708115630', '247833096', '468606247', '931213301', '1020145437', '646960504', '729435957', '735113523', '673263142', '686681834', '238242668', '280079266', '719244813', '684532130', '555363998', '682415486']
 
 
 @run_preprocessor
